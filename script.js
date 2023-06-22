02aa11535034a9813ea31c0006adab1e19956c7a
Thzzao <thiago.santos1160@gmail.com>
2023-06-22 17:37:59 -0300

    Continuação da criação das funções para conversão dos valores de peso e temperatura

--- script.js
+++ script.js
@@ -1,44 +1,66 @@
-// pegando o valor que foi dado pelo usuário para fazer o calculo
-const valor = document.getElementById ("valor").value
+// pegando o valor que foi dado pelo usuário para fazer o cálculo
+const valor = parseFloat (document.getElementById ("valor").value)
+// pegando as categorias e as unidades de medida requeridas para o cálculo
+const categoria = document.getElementById ("categoria").value
+const uniEntrada = document.getElementById ("uniEntrada").value
+const uniSaida = document.getElementById ("uniSaida").value
 
-resultado = document.getElementById ("resultado") //docuemnt.write
-
-var categoria = document.getElementById ("categoria").value
-
-var uniEntrada = document.getElementById ("uniEntrada").value
-
-var uniSaida = document.getElementById ("uniSaida").value
 
 //funcão que realizará o calculo 
 function converter() {
-   // caso nao tenha nada no input
-   if (valor.value == " "){
-      return
-   }
-   // Conversão da string do input para número
-   numero = valor.value
-   numero = parseFloat(numero)
 
 
-   if (categoria.value == "COMP"){
+   //validação dos campos
+   if (categoria === "COMP"){
       if (uniEntrada === "m" & uniSaida === "cm"){
          metrosParaCentimetros()
       }
       else if (uniEntrada === "cm" & uniSaida ==="m"){
          centimetrosParaMetros()
       }
+      else if (uniEntrada === "pol" & uniSaida ==="m"){
+         polegadasParaMetros()
+      }
+      else if (uniEntrada === "m" & uniSaida ==="pol"){
+         metrosParaPolegadas()
+      }
+      else if (uniEntrada === "cm" & uniSaida ==="pol"){
+         centimetrosParaPolegadas()
+      }
+      else if (uniEntrada === "pol" & uniSaida ==="cm"){
+         polegadasParaCentimetros()
+      }
+      else if (uniEntrada === uniSaida){
+         document.getElementById("resultado").textContent = `Selecione uma unidade diferente`;
+      }
+   }
+   else if (categoria === "PESO"){
+      if (uniEntrada === "kg" & uniSaida ==="g"){
+         quiloParaGramas()
+      }
+      else if (uniEntrada === "g" & uniSaida ==="kg"){
+         gramasParaQuilo()
+      }
+      else if (uniEntrada === "kg" & uniSaida ==="li"){
+         quiloParaLibras()
+      }
+      else if (uniEntrada === "li" & uniSaida ==="kg"){
+         librasParaQuilo()
+      }
+      else if (uniEntrada === "g" & uniSaida ==="li"){
+         gramasParaLibras()
+      }
+      else if (uniEntrada === "li" & uniSaida ==="g"){
+         librasParaGramas()
+      }
+   }
+   else { //categoria === "TEMP"
+      if (uniEntrada === "c" & uniSaida ==="f"){
+         celsiusParaFahren()
+      }
       else if
    }
-
-
-   else if (categoria.value == "PESO"){
-
-   }
-   else (categoria.value == "TEMP"){
-
-   }
 }
-
 
 
 // Funções de conversão COMPRIMENTO
@@ -84,27 +106,84 @@ function centimetrosParaPolegadas (valor) {
 }
 
 // Função para converter polegadas para centímetros
-function centimetrosParaPolegadas (valor) { 
+function polegadasParaCentimetros (valor) { 
    const fatorConversao = 2.54; // 1 polegada equivale a 2,54 centímetros 
    const centimetros = valor / fatorConversao;
    // Exibir o resultado na página HTML
    document.getElementById("resultado").textContent = `${valor} polegadas equivalem a ${centimetros} centímetros.`;
 }
 
+
 // Funções de conversão PESO
 
-// Função para converter xxxxx para xxxxxx
-function xxxxxxParaxxxxx (valor) { 
-   const fatorConversao = xxxxxx; // 1 xxxxx equivale a xxxxx xxxxxx 
-   const xxxxxxx = valor * fatorConversao;
+// Função para converter quiligramas para gramas
+function quiloParaGramas (valor) { 
+   const fatorConversao = 1000; // 1 kg equivale a 1000 gramas 
+   const gramas = valor * fatorConversao;
    // Exibir o resultado na página HTML
-   document.getElementById("resultado").textContent = `${valor} xxxxxx equivalem a ${xxxxx} xxxxxxxx.`;
+   document.getElementById("resultado").textContent = `${valor} quilogramas equivalem a ${gramas} gramas.`;
 }
 
-// Função para converter xxxxx para xxxxxx
-function xxxxxxParaxxxxx (valor) { 
-   const fatorConversao = xxxxxx; // 1 xxxxx equivale a xxxxx xxxxxx 
-   const xxxxxxx = valor * fatorConversao;
+// Função para converter gramas para quilogramas
+function gramasParaQuilo (valor) { 
+   const fatorConversao = 1000; // 1 kg equivale a 1000 gramas 
+   const quilos = valor / fatorConversao;
    // Exibir o resultado na página HTML
-   document.getElementById("resultado").textContent = `${valor} xxxxxx equivalem a ${xxxxx} xxxxxxxx.`;
+   document.getElementById("resultado").textContent = `${valor} gramas equivalem a ${quilos} quilogramas.`;
+}
+
+// Função para converter quilogramas para libras
+function quiloParaLibras (valor) { 
+   const fatorConversao = 2.205; // 1 quilograma equivale a 2,205 libras 
+   const libras = valor * fatorConversao;
+   // Exibir o resultado na página HTML
+   document.getElementById("resultado").textContent = `${valor} quilogramas equivalem a ${libras} libras.`;
+}
+
+// Função para converter libras para quilogramas
+function librasParaQuilo (valor) { 
+   const fatorConversao = 2.205; // 1 quilograma equivale a 2,205 libras 
+   const quilos = valor / fatorConversao;
+   // Exibir o resultado na página HTML
+   document.getElementById("resultado").textContent = `${valor} libras equivalem a ${quilos} quilogramas.`;
+}
+
+// Função para converter gramas para libras
+function gramasParaLibras (valor) { 
+   const fatorConversao = 453.6; // 1 grama é igual a 0,0022046 libras 
+   const libras = valor / fatorConversao;
+   // Exibir o resultado na página HTML
+   document.getElementById("resultado").textContent = `${valor} gramas equivalem a ${libras} libras.`;
+}
+
+// Função para converter libras para gramas
+function librasParaGramas (valor) { 
+   const fatorConversao = 453.6; // 1 grama é igual a 0,0022046 libras 
+   const gramas = valor * fatorConversao;
+   // Exibir o resultado na página HTML
+   document.getElementById("resultado").textContent = `${valor} libras equivalem a ${gramas} gramas.`;
+}
+
+
+// Funções de conversão TEMPERATURA
+
+// Função para converter celsius para fahren
+function celsiusParaFahren (valor) { 
+   const fahren = valor * 1.8 + 32 
+   // Exibir o resultado na página HTML
+   document.getElementById("resultado").textContent = `${valor} graus Celsius equivalem a ${fahren} graus Fahrenheit.`;
+}
+
+// Função para converter fahren para celsius
+function fahrenParaCelsius (valor) { 
+   const celsius = (valor - 32) / 1.8 
+   // Exibir o resultado na página HTML
+   document.getElementById("resultado").textContent = `${valor} graus Fahrenheit equivalem a ${celsius} graus Celsius.`;
+}
+
+// Função para converter kelvin para fahren
+function kelvinParaFahren (valor) { 
+   const fahren = 
+   // Exibir o resultado na página HTML
+   document.getElementById("resultado").textContent = `${valor} Kelvin equivalem a ${fahren} graus Fahrenheit.`;
 }
